60ea474373ecdfc91c6db49848844c7239e32535
SACHIN SHARMA <sachinshama3191@gmail.com>
2020-03-03 22:01:57 -0800

    Kindly review Array=4

--- arrayPartition.java
+++ arrayPartition.java
@@ -6,3 +6,15 @@
 
 
 // Your code here along with comments explaining your approach
+
+class Solution {
+    public int arrayPairSum(int[] nums) {
+        Arrays.sort(nums);
+        int sum = 0,i =0;
+        while(i < nums.length){
+            sum += nums[i];
+            i += 2;;
+        }
+        return sum;
+    }
+}
