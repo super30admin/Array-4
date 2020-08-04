195c67caca3d798cfdcf52b17bfe2193628a70a1
Sarthak Sugandhi <47070701+sarthaksugandhi9@users.noreply.github.com>
2020-08-04 13:23:14 -0700

    Create Problem_3.java

---
+++ Problem_3.java
@@ -0,0 +1,33 @@
+// Time complexity - O(n)
+// Space complexity - O(n)
+
+class Solution {
+    public void nextPermutation(int[] nums) {
+        if(nums == null || nums.length == 0) return;
+        int i = nums.length - 2;
+        while(i >= 0 && nums[i] >= nums[i+1]){
+            i--;
+        }
+        if(i >= 0){
+            int j = nums.length - 1;
+            while(nums[i] >= nums[j]){
+                j--;
+            }
+            swap(nums,i,j);
+        }
+        reverse(nums,i+1,nums.length-1);
+    }
+    
+    private void swap(int[] nums, int i , int j){
+        int t = nums[i];
+        nums[i] = nums[j];
+        nums[j] = t; 
+    }
+    
+    private void reverse(int[] nums, int i, int j){
+        while(i < j){
+            swap(nums,i,j);
+            i++; j--;
+        }
+    }
+}
