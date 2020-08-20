813e962da3273420afe12c7eb4d9e0ce4ae22a16
PadmaPriyaCheruvu <65566756+PadmaPriyaCheruvu@users.noreply.github.com>
2020-08-20 14:21:36 -0500

    Update MaxSubarray.java

--- MaxSubarray.java
+++ MaxSubarray.java
@@ -21,3 +21,20 @@ class Solution {
         return max;
         }
     }
+
+
+//Time-O(n)
+//Space-O(1)
+class Solution {
+    public int maxSubArray(int[] nums) {
+        if(nums==null||nums.length==0)
+            return 0;
+        int local = nums[0];
+        int global = local;
+        for(int i=1;i<nums.length;i++){
+            local = Math.max(nums[i],local+nums[i]);
+            global = Math.max(local,global);
+        }
+        return global;
+    }
+}
