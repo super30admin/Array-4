d55f8d16640114f0f31c66bb9873d6e1f9273ae2
Arpit Sharma <arpit317@gmail.com>
2020-09-09 18:45:47 -0700

    Update MaximumSubarray.java

--- MaximumSubarray.java
+++ MaximumSubarray.java
@@ -11,3 +11,25 @@ class Solution {
     }
 }
 
+
+
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int rSum = nums[0]; int max = nums[0];
+        int start = 0; int end = 0; int k = 0;
+        for(int i = 1; i < nums.length; i++){
+            rSum = rSum+nums[i];
+            if(rSum < nums[i]){
+                rSum = nums[i];
+                k = i;
+            }
+            if(rSum > max){
+                max = rSum;
+                start = k;
+                end = i;
+            }
+        }
+        System.out.println(start+" "+ end);
+        return max;
+    }
+}
