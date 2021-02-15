a5661113ba448159c035f71b9de6fef2b70904a1
Rishi Raj <rishiraj824@gmail.com>
2021-02-14 19:29:10 -0500

    Create array-partition.java

---
+++ array-partition.java
@@ -0,0 +1,9 @@
+Time - O(NLgN+N), Space - O(N)
+public int arrayPairSum(int[] nums) {
+        Arrays.sort(nums);
+        int sum = 0;
+        for (int i = 0; i < nums.length; i += 2) {
+            sum += nums[i];
+        }
+        return sum;
+    }
