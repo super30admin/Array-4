40064ab0b46f50ace342b7e26699c865f4c2df54
soumith <soumithr@buffalo.edu>
2021-03-23 11:09:53 -0400

    Finished Task

---
+++ ArrayPartition1.py
@@ -0,0 +1,12 @@
+﻿#TimeComplexity:O(NlogN) for sorting
+#SpaceComplexity: constant space
+#Did this code successfully run on Leetcode : Yes 
+#Any problem you faced while coding this : No
+class Solution:
+    def arrayPairSum(self, nums: List[int]) -> int:
+        nums.sort()
+        sum=0
+        for i in range(len(nums)):
+            if i%2==0:
+                sum+=nums[i]
+        return sum
