ed558190d55fe76c9a36d5b0b547ef477db778f7
omkar <omkarvaidya13@gmail.com>
2021-04-22 20:38:25 -0400

    Completed Array-4

---
+++ NextPermutation.java
@@ -0,0 +1,49 @@
+class NextPermutation {
+    
+    // Time Complexity: O(n)
+    // Space Complexity: O(1)
+    
+    public void nextPermutation(int[] nums) {
+        if(nums == null || nums.length == 0)
+            return;
+        
+        int n = nums.length;
+        int peak_index = -1;
+        int peak_swap_elem_index = -1;
+        
+        // Finding the peak element
+        for(int i = n-2; i >= 0; i--){
+            if(nums[i] < nums[i+1]){
+                peak_index = i;
+                break;
+            }
+        }
+
+        // Find swap element index
+        for(int i = n-1; i >= 0 && peak_index != -1; i--){
+            if(nums[peak_index] < nums[i]){
+                peak_swap_elem_index = i;
+                // Swap
+                swap(nums, peak_index, peak_swap_elem_index);
+                break;
+            }
+        }
+        
+        // Reverse the remaining array after the peak element
+        reverse(nums, peak_index+1, n-1);        
+    }
+    
+    private void swap(int[] nums,int i, int j){
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+    
+    private void reverse(int[] nums, int start, int end){
+        while(start < end){
+            swap(nums, start, end);
+            start++;
+            end--;
+        }
+    }
+}
