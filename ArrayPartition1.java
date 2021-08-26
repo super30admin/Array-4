35cd49eacb529fde74ca031ae97925cd1e4ebfad
Radhika Kulkarni <radhikankulkarni@gmail.com>
2021-08-25 17:19:42 -0700

    Completed Array Partition 1

---
+++ ArrayPartition1.java
@@ -0,0 +1,16 @@
+// TC: O(nlogn)
+// SC: O(1)
+
+class Solution {
+    public int arrayPairSum(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        Arrays.sort(nums);
+        int sum = 0;
+        for(int i = 0; i < nums.length; i += 2) {
+            sum += nums[i];
+        }
+        return sum;
+    }
+}
