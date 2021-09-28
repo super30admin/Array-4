aac99123233219acd6547e20e1156a3febc9b00d
Yash Naredi <yashnaredi26@gmail.com>
2021-09-28 13:13:02 -0400

    completed array 4

---
+++ problem3.py
@@ -0,0 +1,26 @@
+#next permutation
+#tc:O(N) where n is the len of nums 
+#sc: O(1)
+
+
+
+class Solution:
+    def nextPermutation(self, nums):
+        i = j = len(nums)-1
+        while i > 0 and nums[i-1] >= nums[i]:
+            i -= 1
+        if i == 0:   # nums are in descending order
+            nums.reverse()
+            return 
+        k = i - 1    # find the last "ascending" position
+        while nums[j] <= nums[k]:
+            j -= 1
+        nums[k], nums[j] = nums[j], nums[k]  
+        l, r = k+1, len(nums)-1  # reverse the second part
+        while l < r:
+            nums[l], nums[r] = nums[r], nums[l]
+            l +=1 ; r -= 1
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        
