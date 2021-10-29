74f042388cfd0379f3951d0d6a3e24109e5a0241
Aashish Kumar <vats96ashu@gmail.com>
2021-10-28 20:20:23 -0500

    completed sprint Array-4

---
+++ maximumSubArray.java
@@ -0,0 +1,15 @@
+class Solution {
+
+    // TC:O(n) SC:O(1)
+    public int maxSubArray(int[] nums) {
+        if (nums == null || nums.length == 0)
+            return 0;
+        int rsum = nums[0];
+        int max = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            rsum = Math.max(rsum + nums[i], nums[i]);
+            max = Math.max(max, rsum);
+        }
+        return max;
+    }
+}
