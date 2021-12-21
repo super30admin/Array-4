91edd5bd6a48deec34a9be60f99b0b0ac0968c97
Spandana Edupuganti <spandanaedupuganti31@gmail.com>
2021-12-20 20:18:39 -0500

    Array-4 completed

---
+++ MaxSubArray.java
@@ -0,0 +1,24 @@
+// Time Complexity : O(N)
+// Space Complexity : O(1)
+// Did this code successfully run on Leetcode : Yes
+// Any problem you faced while coding this : No
+
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int sumTillNow=0;
+        int maxSum=nums[0];
+        for(int i=0;i<nums.length;i++){
+            sumTillNow+=nums[i];
+            
+            if(sumTillNow>maxSum){
+                maxSum=sumTillNow;
+            }
+            
+            if(sumTillNow<0){
+                sumTillNow=0;
+            }
+        }
+        
+       return maxSum;
+    }
+}
---
+++ PartitionArray.java
@@ -0,0 +1,16 @@
+// Time Complexity : O(NLogN)
+// Space Complexity : O(1)
+// Did this code successfully run on Leetcode : Yes
+// Any problem you faced while coding this : No
+
+class Solution {
+    public int arrayPairSum(int[] nums) {
+        Arrays.sort(nums);
+        int result=0;
+        for(int i=0;i<nums.length;i=i+2){
+            result+=nums[i];
+        }
+        
+        return result;
+    }
+}
