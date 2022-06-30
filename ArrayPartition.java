7bcb9bc910f5fc70ac06a7bbbdf6e969bf384546
Neha Mishra <neha79091@gmail.com>
2022-06-30 07:14:07 +0530

    Array4Done

---
+++ ArrayPartition.java
@@ -0,0 +1,17 @@
+// Time Complexity :O(nlogn)
+// Space Complexity :constant
+// Did this code successfully run on Leetcode :yes
+// Any problem you faced while coding this :No
+
+class Solution {
+    public int arrayPairSum(int[] nums) {
+        // sort the array
+        Arrays.sort(nums);
+        int result = 0;
+        // make pairs starting from index 0 and add min of pair in result
+        for (int i = 0; i < nums.length; i += 2) {
+            result += Math.min(nums[i], nums[i + 1]);
+        }
+        return result;
+    }
+}
---
+++ NextPermutation.java
@@ -0,0 +1,50 @@
+// Time Complexity :O(n)
+// Space Complexity :constant
+// Did this code successfully run on Leetcode :yes
+// Any problem you faced while coding this :no
+class Solution {
+    public void nextPermutation(int[] nums) {
+        boolean isBreachFound = false;
+        int breach = 0;
+        // find the breach
+        for (int i = nums.length - 2; i >= 0; i--) {
+            if (nums[i] < nums[i + 1]) {
+                isBreachFound = true;
+                breach = i;
+                break;
+            }
+        }
+        // if breach is found, find the number just greater than breach
+        if (isBreachFound) {
+            for (int i = nums.length - 1; i >= 0; i--) {
+                if (nums[i] > nums[breach]) {
+                    // if found swap
+                    swap(nums, i, breach);
+                    break;
+                }
+            }
+            // reverse from next to breach till end
+            reverse(nums, breach + 1, nums.length - 1);
+        } else {
+            reverse(nums, 0, nums.length - 1);
+        }
+    }
+
+    // method to swap
+    public void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+
+    // method to reverse
+    public void reverse(int[] nums, int i, int j) {
+        int start = i;
+        int end = j;
+        while (start < end) {
+            swap(nums, start, end);
+            start++;
+            end--;
+        }
+    }
+}
