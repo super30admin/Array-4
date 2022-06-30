deec38a1f979482bef85bc2d4a5047665ff2f086
Abhishek Chaudhary <abhishek.chaudhary.2812@gmail.com>
2022-06-30 00:11:22 -0500

    Completed Array 4 Assignment

---
+++ ArrayPartition.java
@@ -0,0 +1,23 @@
+/**
+Time Complexity : O(N)
+Space Complexity : O(1)
+Did this code successfully run on Leetcode : Yes
+Any problem you faced while coding this : No
+*/
+class Solution 
+{
+    public int arrayPairSum(int[] nums) 
+    {
+        
+        Arrays.sort(nums);
+        
+        int sum = 0;
+        
+        for( int i =0 ; i <nums.length; i = i + 2)
+        {
+            sum += nums[i];
+        }
+        
+        return sum;
+    }
+}
