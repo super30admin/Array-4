a6a33084eb52b78ffd8adcf5b96b2e211138ba87
karthik <shilpaparameshwara1997@gmail.com>
2022-07-03 21:58:35 +0530

    dpne

---
+++ maximum_subarray.java
@@ -0,0 +1,24 @@
+// Time Complexity :O(n)
+// Space Complexity :constant
+// Did this code successfully run on Leetcode :yes
+
+
+class Solution {
+    public int maxSubArray(int[] nums) {
+
+        int currSum = nums[0];
+        int runnSum = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            // add current number to runningSum
+            runnSum += nums[i];
+            // if current number is greater than runningSum, start runningsum from curr
+            // number
+            if (runnSum < nums[i]) {
+                runnSum = nums[i];
+            }
+            // store maximum in result
+            currSum = Math.max(currSum, runnSum);
+        }
+        return currSum;
+    }
+}
