45c102ecfad8e3739769fde200673ed8db5b061e
akshaykmus <akshaykmus@gmail.com>
2022-07-28 08:30:42 -0400

    Completed Array Partition problem

---
+++ arraypartition.java
@@ -0,0 +1,15 @@
+//Time : O(nlogn)
+//Space :O(1)
+
+class Solution {
+    public int arrayPairSum(int[] nums) {
+        if (nums == null || nums.length == 0)
+            return 0;
+        int result = 0;
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length; i += 2) {
+            result += nums[i];
+        }
+        return result;
+    }
+}
