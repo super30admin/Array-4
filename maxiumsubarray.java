7be1023250df60df4fa9baadd62e537e9a96637a
akshaykmus <akshaykmus@gmail.com>
2022-07-28 21:08:41 -0400

    Completed maximum subarray problem

---
+++ maxiumsubarray.java
@@ -0,0 +1,17 @@
+//T : O(n)
+//S :O(1)
+
+class Solution {
+
+    public int maxSubArray(int[] nums) {
+        if (nums == null || nums.length == 0)
+            return 0;
+        int currSum = nums[0];
+        int max = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            currSum = Math.max(nums[i], currSum + nums[i]);
+            max = Math.max(max, currSum);
+        }
+        return max;
+    }
+}
