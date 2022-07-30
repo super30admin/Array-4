d6c8646515f9147132b4a16c99d82796580af536
rahul-gajwani <rahul.gajwani10@gmail.com>
2022-07-30 22:26:23 +0530

    Create MaximumSubArray.java

---
+++ MaximumSubArray.java
@@ -0,0 +1,31 @@
+//TC - O(n)
+//SC - O(1)
+class Solution {
+    public int maxSubArray(int[] nums) {
+        if(nums == null || nums.length == 0) return 0;
+        int maxSum = nums[0];
+        int currSum = nums[0];
+        int start = 0;
+        int end = 0;
+        int currStart = 0;
+        for(int i = 1; i < nums.length; i++){
+            if(nums[i] > currSum + nums[i])
+            {
+                currStart = i;
+                currSum = nums[i];
+            }
+            else
+            {
+                currSum = currSum + nums[i];
+            }
+            if(currSum > maxSum){
+                maxSum = currSum;
+                start = currStart;
+                end = i;
+            }
+        }
+        System.out.println("startIndex" + start);
+        System.out.println("endindex" + end);
+        return maxSum;
+    }
+}
