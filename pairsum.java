3491ab06c35c41d6f4c49e3cc6b4e9e1834ed6bc
Hanuman Reddy <39452135+HanumanReddy1212@users.noreply.github.com>
2022-08-01 20:42:57 -0700

    array-4 done

---
+++ pairsum.java
@@ -0,0 +1,17 @@
+//TC : O(nlogn)
+//SC : O(1)
+
+class Solution {
+    public int arrayPairSum(int[] nums) {
+        if(nums == null || nums.length == 0)return 0;
+
+        int result = 0;
+        Arrays.sort(nums);
+
+        for(int i = 0; i< nums.length; i = i + 2){
+            result += nums[i];
+        }
+
+        return result;
+    }
+}
