0e7c3363d18fcc337091ce0c50df98c723750ddc
rutujamandlikFT <123407038+rutujamandlikFT@users.noreply.github.com>
2023-03-28 22:20:09 -0400

    Update arrayPartition.java

--- arrayPartition.java
+++ arrayPartition.java
@@ -1,5 +1,13 @@
-//TC: O(nlogn)
-//SC:O(1)
+//Time Complexity : O(NlogN) 
+//Space Complexity : O(1) 
+//Did this code successfully run on Leetcode : Yes
+//Any problem you faced while coding this : No
+
+/**
+ * Sort the array in ascending order and pick the even elements to add. return
+ * the total even sum.
+ *
+ */
 class Solution {
     public int arrayPairSum(int[] nums) {
         if(nums == null || nums.length == 0) return 0;
