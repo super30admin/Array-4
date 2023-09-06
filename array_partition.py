f50b4b33e40202a5b12d66912b9205f7315d72ad
NisargaVenkatesh <venkatesh.ni@northeastern.edu>
2023-09-06 14:45:43 -0400

    Exercise for review

--- array_partition.py
+++ array_partition.py
@@ -2,8 +2,10 @@
 #SC:O(1)
 class Solution:
     def arrayPairSum(self, nums):
-        nums.sort()  # Sort the array in ascending order.
+         # Sort the array in ascending order.
+        nums.sort() 
         total = 0
         for i in range(0, len(nums), 2):
-            total += nums[i]  # Add the minimum value from each pair to the total.
+            # Add the minimum value from each pair to the total.
+            total += nums[i]  
         return total
