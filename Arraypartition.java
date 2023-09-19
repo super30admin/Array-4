6b9895840dac906552e86698d7ec5447e60dcd91
Vishwesh130320 <s.vishwesh1303@gmail.com>
2023-09-19 09:32:29 -0700

    Completed Array-4

---
+++ Arraypartition.java
@@ -0,0 +1,13 @@
+//Time: O(nlogn), because we are sorting the array
+//Space: O(1)
+
+class Solution {
+    public int arrayPairSum(int[] nums) {
+        Arrays.sort(nums);
+        int max = 0;
+        for(int i =0 ; i<nums.length; i+=2){
+            max+= nums[i];
+        }
+        return max;
+    }
+}
