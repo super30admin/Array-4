499461a3bde83164639a2db63d86c22340b11762
rkpal1207 <palrekha1995@gmail.com>
2024-03-11 14:47:23 -0500

    DONE

---
+++ problem3.py
@@ -0,0 +1,28 @@
+#Time Complexity : O(N)
+#Space Complexity :O(1)
+#Did this code successfully run on Leetcode :yes
+#Any problem you faced while coding this : No
+from ast import List
+
+
+class Solution:
+    def nextPermutation(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        i = len(nums) - 2
+        while i >= 0 and nums[i] >= nums[i+1]:
+            i -= 1
+        
+        if i >= 0:
+            j = len(nums) - 1
+            while nums[j] <= nums[i]:
+                j -= 1
+            nums[i], nums[j] = nums[j], nums[i]
+        
+        left, right = i+1, len(nums) - 1
+        while left < right:
+            nums[left], nums[right] = nums[right], nums[left]
+            left += 1
+            right -= 1
+        
